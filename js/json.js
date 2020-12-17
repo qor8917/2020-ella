ea46f3be328d44d0c30b5d605c11e892f836a3f7
BK SEONG <qor8917@gmail.com>
2020-12-17 18:21:00 +0900

    2020/12/17 수업끝

---
+++ js/json.js
@@ -0,0 +1,67 @@
+var products = [{
+    id: 1,
+    link: "#",
+    title: "MARC",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 5.0,
+    originalPrice: 199.00,
+    salePrice: 139.00,
+    origin: "from",
+    src: "../img/pick1.jpg"
+},
+{
+    id: 2,
+    link: "#",
+    title: "G-STAR",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 0,
+    originalPrice: 189.00,
+    salePrice: 0,
+    origin: "",
+    src: "../img/pick2.jpg"
+},
+{
+    id: 3,
+    link: "#",
+    title: "CHANEL",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 5,
+    originalPrice: 215.00,
+    salePrice: 192.10,
+    origin: "",
+    src: "../img/pick3.jpg"
+},
+{
+    id: 4,
+    link: "#",
+    title: "BURBERRY",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 3,
+    originalPrice: 189.00,
+    salePrice: 0,
+    origin: "",
+    src: "../img/pick4.jpg"
+},
+{
+    id: 5,
+    link: "#",
+    title: "BURBERRY",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 3,
+    originalPrice: 90.00,
+    salePrice: 139.00,
+    origin: "from",
+    src: "../img/pick5.jpg"
+},
+{
+    id: 6,
+    link: "#",
+    title: "BENJAMIN BUTTON",
+    summary: "Dinterdum Pretium De Condimentus",
+    star: 5,
+    originalPrice: 189.00,
+    salePrice: 139.00,
+    origin: "from",
+    src: "../img/pick6.jpg"
+}
+]
